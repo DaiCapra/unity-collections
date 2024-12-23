8e31dbf0a5e2894aca2403959bf02b15d3974290
Daniel Artmann <daniel@artmann.co>
2024-12-23 22:39:58 +0100

    fix: implicit

--- Assets/Packages/DataForge/Scripts/Runtime/Data/STransform.cs
+++ Assets/Packages/DataForge/Scripts/Runtime/Data/STransform.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace DataForge.Data
 {
     public struct STransform
@@ -5,5 +7,23 @@ namespace DataForge.Data
         public SVector3 position;
         public SVector3 rotation;
         public SVector3 scale;
+
+
+        public static implicit operator STransform(Transform t)
+        {
+            return new()
+            {
+                position = t.position,
+                rotation = t.rotation.eulerAngles,
+                scale = t.localScale
+            };
+        }
+
+        public void Apply(Transform transform)
+        {
+            transform.position = position;
+            transform.rotation = Quaternion.Euler(rotation);
+            transform.localScale = scale;
+        }
     }
 }
