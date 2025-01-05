910a443496cd6da7d1363cc88c5abcac92f5b6c8
Daniel Artmann <daniel@artmann.co>
2025-01-05 17:36:06 +0100

    fix: added attribute check to reflectionmanager

--- Assets/Packages/DataForge/Scripts/Runtime/Reflection/ReflectionManager.cs
+++ Assets/Packages/DataForge/Scripts/Runtime/Reflection/ReflectionManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace DataForge.Reflection
@@ -8,15 +9,7 @@ namespace DataForge.Reflection
     {
         private static readonly Dictionary<Type, Dictionary<string, Member>> MemberMap = new();
 
-        public static Member GetMember(Type type, string name)
-        {
-            var members = GetMembers(type);
-            return members.TryGetValue(name, out var member)
-                ? member
-                : null;
-        }
-
-        public static Dictionary<string, Member> GetMembers(Type type)
+        public static Dictionary<string, Member> GetMembers<T>(Type type) where T : Attribute
         {
             if (MemberMap.TryGetValue(type, out var members))
             {
@@ -26,14 +19,19 @@ namespace DataForge.Reflection
             var map = new Dictionary<string, Member>();
 
             var flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
-            var fields = type.GetFields(flags);
+            var fields = type
+                .GetFields(flags)
+                .Where(t => t.GetCustomAttribute<T>() != null);
 
             foreach (var field in fields)
             {
                 map[field.Name] = new FieldMember(field);
             }
 
-            var properties = type.GetProperties(flags);
+            var properties = type
+                .GetProperties(flags)
+                .Where(t => t.GetCustomAttribute<T>() != null);
+
             foreach (var property in properties)
             {
                 map[property.Name] = new PropertyMember(property);
